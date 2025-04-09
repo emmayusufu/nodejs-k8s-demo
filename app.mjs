083f2a607a75e199c6aa73c0ce84a2de0b7c5f3a
Emmanuel Yusufu Kimaswa <kimaswaemma36@gmail.com>
2025-04-09 23:14:40 +0300

    update welcome message

--- app.mjs
+++ app.mjs
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Welcome");
+  res.send("Welcome to kubernetes");
 });
 
 app.get("/greeting", (req, res) => {
